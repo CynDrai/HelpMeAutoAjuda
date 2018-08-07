0c29ec860a944bb97133e0992a7a96d10a855bfa
CynDrai <cyndrai@users.noreply.github.com>
2018-08-07 12:13:40 -0300

    Atualização Interface e Java
    -Adicionado em texts de TextView e Button XML a função @string/"valor" para que os valores vão para string.xml e possam ser reútilizados;
    
    -Adicionado ID em todos os TextView;
    
    -Retirado posições demarcadas no XML e colocado android:layout_bellow em todos os TextView e EditText;
    
    -Adicionado Toast para Login;
    
    -Adicionado Intent para cadastro para mudar a Activity para CadastroActivity;

--- app/src/main/java/com/example/felipesavaris/helpmeautoajuda/MainActivity.java
+++ app/src/main/java/com/example/felipesavaris/helpmeautoajuda/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.felipesavaris.helpmeautoajuda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginBt (View view) {
+        Toast.makeText(
+                this,
+                "Método não suportado ainda",
+                Toast.LENGTH_LONG).show();
+    }
+
+    public void cadastroBt (View view) {
+
+        //Mudança de Activity --> CadastroActivity
+        Intent it = new Intent(
+                this,
+                CadastroActivity.class);
+
+        startActivity(it);
     }
     
 }
