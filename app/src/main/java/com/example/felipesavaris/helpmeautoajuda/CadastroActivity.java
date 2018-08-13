f6cb71d7a27a3600f05066d960b67283e1c1fc70
CynDrai <cyndrai@users.noreply.github.com>
2018-08-13 17:41:06 -0300

    Criação de Base para cadastro
    -Criação de Inits para EditText;
    
    -Criação de dois métodos abstrátos DAO

--- app/src/main/java/com/example/felipesavaris/helpmeautoajuda/CadastroActivity.java
+++ app/src/main/java/com/example/felipesavaris/helpmeautoajuda/CadastroActivity.java
@@ -2,6 +2,7 @@ package com.example.felipesavaris.helpmeautoajuda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
@@ -14,6 +15,15 @@ public class CadastroActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
     }
 
+    public void cadastroBt(View view) {
 
+        //Inicialização dos EditText
+        this.edEmail = (EditText) findViewById(R.id.edEmail);
+        this.edNameUsr = (EditText) findViewById(R.id.edNameUsr);
+        this.edNameFan = (EditText) findViewById(R.id.edNameFan);
+        this.edSenhaUsuario = (EditText) findViewById(R.id.edSenhaUsuario);
+
+
+    }
 
 }
--- app/src/main/java/com/example/felipesavaris/helpmeautoajuda/DAO/AbstractDAO.java
+++ app/src/main/java/com/example/felipesavaris/helpmeautoajuda/DAO/AbstractDAO.java
@@ -2,6 +2,8 @@ package com.example.felipesavaris.helpmeautoajuda.DAO;
 
 public abstract class AbstractDAO {
 
+    //Login
+    public abstract void addLogin();
 
-
+    public abstract void findLogin();
 }
