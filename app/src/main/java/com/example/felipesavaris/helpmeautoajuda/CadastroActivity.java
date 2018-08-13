74619911523846a2d12417c11dccee101212550c
CynDrai <cyndrai@users.noreply.github.com>
2018-08-13 15:57:10 -0300

    Inicio do cadastro e correção
    -Adicionado o Package DAO junto com uma classe abstrata AbstractDAO.java;
    
    -Mudança dos ID's nos XML para uma melhor comprensão no java: XML afetados - activity_main.xml, activity_cadastro.xml

--- app/src/main/java/com/example/felipesavaris/helpmeautoajuda/CadastroActivity.java
+++ app/src/main/java/com/example/felipesavaris/helpmeautoajuda/CadastroActivity.java
@@ -2,12 +2,18 @@ package com.example.felipesavaris.helpmeautoajuda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    private EditText edEmail, edNameUsr, edNameFan, edSenhaUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
     }
+
+
+
 }
