2d8220177900eb2cbb9177592343f38a9e768f33
CynDrai <cyndrai@users.noreply.github.com>
2018-10-15 18:50:58 -0300

    Implementação de funcionalidade para ListView
    -Adicionado alfabeto no listView apenas para mostrar sua funcionalide em selectedCategoriaActivity.java;(TEMPORÁRIO)

--- app/src/main/java/com/example/felipesavaris/helpmeautoajuda/selectedCategoriaActivity.java
+++ app/src/main/java/com/example/felipesavaris/helpmeautoajuda/selectedCategoriaActivity.java
@@ -3,6 +3,8 @@ package com.example.felipesavaris.helpmeautoajuda;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class selectedCategoriaActivity extends AppCompatActivity {
 
@@ -10,6 +12,18 @@ public class selectedCategoriaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selected_categoria);
+
+        //CÓDIGO TEMPORÁRIO
+        String test[] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O",
+        "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+        ListView listaTeste = findViewById(R.id.lstRelatos);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, test);
+
+        listaTeste.setAdapter(adapter);
+        //CÓDIGO TEMPORÁRIO
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Depressão");
